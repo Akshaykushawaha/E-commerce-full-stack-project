f504b7e615ff6d065ed9ded57ac0f3664f696718
Akshaykushawaha <akshay.kushawaha@gmail.com>
2024-10-19 02:37:36 +0530

    up

--- docker_app/Flaskapp/app.py
+++ docker_app/Flaskapp/app.py
@@ -23,11 +23,21 @@ try:
         if not es.ping():
             print("Using elasticsearch as: localhost:9200...")
             es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
-            if not es.ping():
-                print("Using elasticsearch as: 172.31.26.196:9200...")
-                es = Elasticsearch([{'host': '172.31.26.196', 'port': 9200, 'scheme': 'http'}])
+            try:
+                response = es.info()
+                print("Elasticsearch is reachable:", response)
+            except Exception as e:
+                print(f"Error connecting to Elasticsearch: {e}")
                 if not es.ping():
-                    raise ValueError("Connection to Elasticsearch failed")
+                    print("Using elasticsearch as: 172.31.26.196:9200...")
+                    es = Elasticsearch([{'host': '172.31.26.196', 'port': 9200, 'scheme': 'http'}])
+                    try:
+                        response = es.info()
+                        print("Elasticsearch is reachable:", response)
+                    except Exception as e:
+                        print(f"Error connecting to Elasticsearch: {e}")
+                        if not es.ping():
+                            raise ValueError("Connection to Elasticsearch failed")
 except Exception as e:  # Catching all exceptions
     print(f"Error connecting to Elasticsearch: {e}")
     es = None  # Disable logging to Elasticsearch if it fails
@@ -78,6 +88,8 @@ def create_app(test_config=None):
     # Routes
     @app.route('/')
     def home():
+        print(os.getcwd())
+        print(os.listdir(os.getcwd()))
         products = mongo.db.products.find()
         # log_to_elk("Home page accessed")
         return render_template('home.html', products=products)
